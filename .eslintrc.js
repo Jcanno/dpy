68e29084b1bedfff40a5b9e4e0fd79b2582e1883
Jacano <641283231@qq.com>
2020-04-14 16:49:00 +0800

    feat:add deploy file feature

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+		es6: true, 
+		node: true, 
+		browser: true
+	},
+  extends: [
+		"eslint:recommended",
+    "scales"
+	],
+	rules: {
+		"no-empty-function": 0
+	},
+  parserOptions: {
+    ecmaVersion: 6,
+    parser: "babel-eslint",
+    sourceType: "module",
+	},
+};
